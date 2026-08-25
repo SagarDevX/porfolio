879f51ffeb98e191ccdc20ddcc0001324bfa0008
Sagar <sagarpundir25@gmail.com>
2026-08-25 19:36:49 +0530

    make the contact page responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -62,7 +62,7 @@ const page = () => {
                     <h1 className="text-2xl text-primary font-bold text-shadow-xs tracking-tight leading-tight">Contact Me</h1>
                     <p className="text-secondary text-md leading-tight ">Feel free to get in touch. Let’s talk about ideas, projects, design, or whatever you’re working on.</p>
                 </div>
-                <form onSubmit={onSubmit} className='flex flex-col gap-6 py-8 px-12 my-12 mx-28 border border-neutral-300 dark:border-neutral-600 rounded-2xl outline outline-offset-4 outline-neutral-300 dark:outline-neutral-600 '>
+                <form onSubmit={onSubmit} className='flex flex-col gap-6 py-8 md:px-12 px-8 my-16 md:mx-28 border border-neutral-300 dark:border-neutral-600 rounded-2xl outline outline-offset-4 outline-neutral-300 dark:outline-neutral-600 '>
                     <div className='w-full flex flex-col gap-1 '>
                         <h4 className='text-primary text-sm'>Full Name</h4>
                         <input type="text" name="name" required className='shadow-sm dark:shadow-[0_1px_5px_rgba(120,120,120,0.2)] p-1 bg-neutral-50 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 rounded-lg text-primary font-medium' />
@@ -73,13 +73,13 @@ const page = () => {
                     </div>
                     <div className='w-full flex flex-col gap-1 '>
                         <h4 className='text-primary text-sm'>Message</h4>
-                        <textarea name="message" required className='shadow-sm dark:shadow-[0_1px_5px_rgba(120,120,120,0.2)] p-1 bg-neutral-50 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 rounded-lg text-primary font-medium'></textarea>
+                        <textarea name="message" required className='min-h-24 shadow-sm dark:shadow-[0_1px_5px_rgba(120,120,120,0.2)] p-1 bg-neutral-50 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 rounded-lg text-primary font-medium'></textarea>
                     </div>
                     <button type="submit" className='inset-shadow-xs inset-shadow-neutral-200 dark:inset-shadow-neutral-700 shadow-sm dark:shadow-[0_1px_5px_rgba(120,120,120,0.5)] p-1 bg-neutral-50 dark:bg-neutral-800 hover:bg-neutral-100 dark:hover:bg-neutral-900 dark:border-neutral-700 rounded-lg text-primary font-xs text-sm transition-all duration-300 ease-in-out cursor-pointer'>Send Message</button>
                     <span>{result}</span>
                 </form>
 
-                <div className='flex flex-col items-center justify-between gap-1 pt-7 border-t border-neutral-300'>
+                <div className='flex flex-col items-center justify-between gap-1 pt-7 my-2 border-t border-neutral-300 '>
                     <h4 className='text-secondary text-md '>Find me on socials -</h4>
                     <div className='flex flex-row items-center gap-4'>
                         {socialMedia.map(({ icon: Icon, href }, idx) => (
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,12 +6,12 @@ import BlogFooter from '../blog/Blog-footer'
 const page = () => {
     return (
         <Container>
-            <div className='pb-1 '>
+            <div className='min-h-screen flex flex-col py-4  '>
                 <div className="flex flex-col gap-0 pt-2 mb-8">
                     <h1 className="text-2xl text-primary font-bold text-shadow-xs tracking-tight leading-tight">Projects</h1>
                     <p className="text-secondary text-md leading-tight ">A collection of things I have built while learning, experimenting, and slowly finding my own way of designing and developing.</p>
                 </div>
-                <div>
+                <div className='py-2'>
                     <Projects/>
                 </div>
                 <BlogFooter/>
